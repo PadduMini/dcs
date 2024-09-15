90400cb7ab5d5a5bb87548025f19383bbf8d0097
PadduMini <paddu.kolli@gmail.com>
2024-09-15 03:51:38 -0700

    Update models.py

--- backend/dcsBackend/docManApi/models.py
+++ backend/dcsBackend/docManApi/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class document(models.Model):
+    document_id = models.IntegerField()
+    document_name = models.CharField(max_length=10)
+    document_type = models.CharField(max_length=10)
